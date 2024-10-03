596f6e2de7798c7b529a1775f1e71c08912ff443
chandanmaheshwari753 <chandanmaheshwari753@gmail.com>
2024-10-03 18:43:37 +0530

    Initial commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,27 +3,28 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-  title: "Promptopia",
-  description: "Discover & Share AI Prompts",
+    title: "Promptopia",
+    description: "Discover & Share AI Prompts",
+    icons: "/assets/images/logo.svg",
 };
 
 const RootLayout = ({ children }) => {
-  return (
-    <html lang="en">
-      <body>
-        <Provider>
-          <div className="main">
-            <div className="gradient" />
-          </div>
+    return (
+        <html lang="en">
+            <body>
+                <Provider>
+                    <div className="main">
+                        <div className="gradient" />
+                    </div>
 
-          <main className="app">
-            <Nav />
-            {children}
-          </main>
-        </Provider>
-      </body>
-    </html>
-  );
+                    <main className="app">
+                        <Nav />
+                        {children}
+                    </main>
+                </Provider>
+            </body>
+        </html>
+    );
 };
 
 export default RootLayout;
