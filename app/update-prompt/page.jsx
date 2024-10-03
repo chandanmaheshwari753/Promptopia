1fccd34dd807c3fc923e0b87f0f1daa80b6f7dc3
chandanmaheshwari753 <chandanmaheshwari753@gmail.com>
2024-10-03 19:43:05 +0530

    updates

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
@@ -10,24 +10,28 @@ const UpdatePrompt = () => {
     const searchParams = useSearchParams();
     const promptId = searchParams.get("id");
 
-    const [submitting, setSubmitting] = useState(false);
     const [post, setPost] = useState({ prompt: "", tag: "" });
+    const [submitting, setIsSubmitting] = useState(false);
 
     useEffect(() => {
         const getPromptDetails = async () => {
             const response = await fetch(`/api/prompt/${promptId}`);
             const data = await response.json();
 
-            setPost({ prompt: data.prompt, tag: data.tag });
+            setPost({
+                prompt: data.prompt,
+                tag: data.tag,
+            });
         };
+
         if (promptId) getPromptDetails();
     }, [promptId]);
 
     const updatePrompt = async (e) => {
         e.preventDefault();
-        setSubmitting(true);
+        setIsSubmitting(true);
 
-        if (!promptId) return alert("Prompt Id not found !");
+        if (!promptId) return alert("Missing PromptId!");
 
         try {
             const response = await fetch(`/api/prompt/${promptId}`, {
@@ -44,20 +48,18 @@ const UpdatePrompt = () => {
         } catch (error) {
             console.log(error);
         } finally {
-            setSubmitting(false);
+            setIsSubmitting(false);
         }
     };
 
     return (
-        <div>
-            <Form
-                type="Edit"
-                post={post}
-                setPost={setPost}
-                submitting={submitting}
-                handleSubmit={updatePrompt}
-            />
-        </div>
+        <Form
+            type="Edit"
+            post={post}
+            setPost={setPost}
+            submitting={submitting}
+            handleSubmit={updatePrompt}
+        />
     );
 };
 
